a193f992d2b42e989f5cf2f3d1db1180f6b265d7
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2024-09-06 19:02:27 +0200

    Do not override "n_values" (rename to "size")
    and rename "param_combination" to "combination"

--- R/calculate_bear1d.R
+++ R/calculate_bear1d.R
@@ -53,14 +53,14 @@ calculate_bear1d <- function(
   # Loop through combinations
   result <- lapply(seq_len(nrow(combinations)), function(i) {
 
-    param_combination <- combinations[i, ]
+    combination <- combinations[i, ]
 
     # Extract individual parameter values
-    n <- param_combination$n
-    rs <- param_combination$rs
-    foc <- param_combination$foc
-    log_koc <- param_combination$log_koc
-    hl <- param_combination$hl
+    n <- combination$n
+    rs <- combination$rs
+    foc <- combination$foc
+    log_koc <- combination$log_koc
+    hl <- combination$hl
 
     # Calculate other parameters
     koc <- 10^(log_koc)
@@ -70,20 +70,20 @@ calculate_bear1d <- function(
     k <- 0.693 / hl #lambda aqueous phase decay constant [1/T]
 
     # Create a data frame for this parameter combination
-    n_values <- length(t_values) * length(x_values)
+    size <- length(t_values) * length(x_values)
 
     data.frame(
       Cx = calculate_Cx(x_values, t_values, v_values, D_values, retardation, C0, k),
       time = rep(t_values, length(x_values)),
-      retardation = rep(retardation, n_values),
-      v = rep(v_values, n_values),
-      hl = rep(hl, n_values),
-      koc = rep(koc, n_values),
-      foc = rep(foc, n_values),
+      retardation = rep(retardation, size),
+      v = rep(v_values, size),
+      hl = rep(hl, size),
+      koc = rep(koc, size),
+      foc = rep(foc, size),
       distance = rep(x_values, each = length(t_values)),
-      dispersion = rep(D_values, each = n_values),
-      Kd = rep(kd, n_values),
-      log_koc = rep(log_koc, n_values)
+      dispersion = rep(D_values, each = size),
+      Kd = rep(kd, size),
+      log_koc = rep(log_koc, size)
     )
 
   })
